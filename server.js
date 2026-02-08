1dad66515ebdde737dfce5cce9b8507c2de8eb83
akhilchandps <akhilchandps@gmail.com>
2026-02-08 15:37:28 +0530

    Fix cors

--- server.js
+++ server.js
@@ -14,9 +14,8 @@ app.use(express.json());
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "https://hospital-appointment-backend-zrhj.onrender.com"    
-  ],
+    "http://localhost:5173"
+    ],
   credentials: true
 }));
 
